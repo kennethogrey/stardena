d3931db1ee9b54af60ea3719cbc56d449c11c6b2
Eluk-Samuel-Kiira <samuelkiiraeluk@gmail.com>
2024-02-23 06:42:34 -0800

    structuring folders

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Home\LandingPageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [LandingPageController::class, 'index']);
+Route::prefix('welcome')->namespace('Home')->group(function () {
+    // Route::get('/', 'LandingPageController@index');
 });
